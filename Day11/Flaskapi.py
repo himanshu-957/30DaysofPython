3464642ea8b871457c2e407c9eb1306643442129
himanshu-957 <himanshujadon30@gmail.com>
2021-12-31 21:13:46 +0530

    Create Flaskapi.py

---
+++ Day11/Flaskapi.py
@@ -0,0 +1,37 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+@app.route("/horoscope/<string:n>")
+def horoscope(n):
+    import urllib.request
+    from bs4 import BeautifulSoup as bs
+    import re
+    import pandas as pd
+    page = urllib.request.urlopen("https://www.horoscope.com/us/index.aspx")
+    soup = bs(page)
+
+    names = soup.body.findAll('a')
+    function_names = re.findall('id="src-hp-.\w+"',str(names))
+    function_names = [item[11:] for item in function_names]
+    function_names = [item[:-1] for item in function_names]
+    function_names.remove('dpo')
+    description = soup.body.findAll('p')
+    function_usage =[]
+
+    for item in description:
+        item = item.text
+        function_usage.append(item)
+    s = 0
+
+    for i in range(0,12):
+        if function_names[i] == n:
+            s = i
+
+#print(len(function_names))
+#print(len(function_usage[:12]))
+    date = function_usage[:12]
+    data = {'Zodiac': function_names[s], 'Date': date[s]}
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(debug=True)
