f5898a0ecce33a7c02ef88c1b3bdbc9e4f9e87a6
himanshu-957 <himanshujadon30@gmail.com>
2022-01-03 13:20:28 +0530

    Update Testobot.py

--- Day13/Testobot.py
+++ Day13/Testobot.py
@@ -1,12 +1,30 @@
 import nextcord
 from nextcord.ext import commands
+import botoken
 clients = commands.Bot(command_prefix="!")
 
 @clients.command()
 async def ping(ctx):
     await ctx.reply("Pong!!")
-from flask import Flask, jsonify
-app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@clients.command()
+async def hello(ctx):
+    await ctx.reply("Hey! I am {0.user}".format(clients))
+    await ctx.reply("How are you?")
+
+
+@clients.command()
+async def fine(ctx):
+    await ctx.reply("That's Great")
+    await ctx.reply("Nice to meet you")
+
+@clients.command()
+async def same(ctx):
+    await ctx.reply(":)")
+
+#@clients.event
+#async def on_message(message):
+#    if message.content.startswith("Fine"):
+#        channel = message.channel
+#        channel.send("That's great")
+clients.run(botoken.TOKEN)
