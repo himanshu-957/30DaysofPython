6545698787cc5da0c2b0c7584c8d1e1298a98adb
himanshu-957 <himanshujadon30@gmail.com>
2022-01-15 14:31:51 +0530

    Update Testobot.py

--- Day13/Testobot.py
+++ Day13/Testobot.py
@@ -1,27 +1,38 @@
 import nextcord
 from nextcord.ext import commands
 import botoken
+import random
+import aiohttp
 clients = commands.Bot(command_prefix="!")
 
 @clients.command()
 async def ping(ctx):
-    await ctx.reply("Pong!!")
+    await ctx.send("Pong!!")
 
 @clients.command()
 async def hello(ctx):
-    await ctx.reply("Hey! I am {0.user}".format(clients))
-    await ctx.reply("How are you?")
+    await ctx.send("Hey! I am {0.user}".format(clients))
+    await ctx.send("How are you?")
 
 
 @clients.command()
 async def fine(ctx):
-    await ctx.reply("That's Great")
-    await ctx.reply("Nice to meet you")
+    await ctx.send("That's Great")
+    await ctx.send("Nice to meet you")
 
 @clients.command()
 async def same(ctx):
-    await ctx.reply(":)")
+    await ctx.send(":)")
 
+@clients.command(pass_context=True)
+async def meme(ctx):
+    embed = nextcord.Embed(title="", description="")
+
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get('https://www.reddit.com/r/dankmemes/new.json?sort=hot') as r:
+            res = await r.json()
+            embed.set_image(url=res['data']['children'] [random.randint(0, 25)]['data']['url'])
+            await ctx.send(embed=embed)
 #@clients.event
 #async def on_message(message):
 #    if message.content.startswith("Fine"):
